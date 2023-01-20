88193ec1440a0f0d07f3ad0347b6d9f1f8bf0d39
Claus <claus.rosenberger@rocnet.de>
2023-01-21 00:22:24 +0100

    Initial Release

---
+++ README.md
@@ -0,0 +1,77 @@
+![Logo](admin/domiqbase.png)
+# ioBroker.domiqbase
+
+[![NPM version](https://img.shields.io/npm/v/iobroker.domiqbase.svg)](https://www.npmjs.com/package/iobroker.domiqbase)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.domiqbase.svg)](https://www.npmjs.com/package/iobroker.domiqbase)
+![Number of Installations](https://iobroker.live/badges/domiqbase-installed.svg)
+![Current version in stable repository](https://iobroker.live/badges/domiqbase-stable.svg)
+
+[![NPM](https://nodei.co/npm/iobroker.domiqbase.png?downloads=true)](https://nodei.co/npm/iobroker.domiqbase/)
+
+## Domiq Base adapter for ioBroker
+
+Domiq Base is home automation device to be used with LCN of the company Issendorff. The adapter will connect via tcp/4224 to the Domiq Base and provide the possibility to get data from the Base and to provide states to the Base. In both directions the objects or states are selected by a list of wildcards or regular expressions. ioBroker states will be published in the Domiq Base as variables. A set of regular expressen to search and a replacement string will ensure to map the ID's as preferred by the user.
+
+### Populating ioBroker with Domiq Base states
+
+To subscribe stated from the Domiq Base you have to create regular expressions to match the state ID's. A predefined set is enabled by default. You have to enable read or write permissions and to set the data-type which has to be used for the states in ioBroker. All values from the Base are received as string. Data will be converted based on the data-type setting in the configuration.  
+#### Examples or a regular expressions ...
+>- `^LCN\.relay\..*$`  
+>(match states of all relays)  
+>  
+>- `^LCN\.relay\.[^\.]+\.[^\.]+\.1$`  
+>(match first state of all relays)  
+>  
+>- `^LCN\.relay\.0\.[^\.]+\.*$`  
+>(match states of all relays in segment 0)  
+  
+There is no syntax check implemented before commands will be sent to the Base.
+
+### Control ioBroker states from the Base
+
+Because only variables can be used to transfer data the state ID's of ioBroker will be mapped to variables in the Domiq Base. To have full control how this mapping will be done a bit regular expression is necessary again. On top a subscriber list has to be configured to avoid every information will be shared with the Base. The stated will be filtered by a two step approach. 
+
+1. Based on the object search list the foreign states will be subscribed
+2. Based on regular expressions a second filter is possible, the mapping is done with groups in the expressions and a replacement string
+
+#### Examples of search strings
+`zigbee.0.*.state`  
+`zigbee.0.*.available`  
+`zigbee.0.*.temperature`  
+#### Examples of regular expressions and replacement strings
+`zigbee\.0\.(.*)` -> iob.zigbee.$1  
+  
+In this configuration table it can be decided to store the value in MEM or VAR variables also.
+
+
+
+## Changelog
+<!--
+    Placeholder for the next version (at the beginning of the line):
+    ### **WORK IN PROGRESS**
+-->
+### 0.1.2 (2023-01-20)
+* (Claus Rosenberger) initial release
+
+## License
+MIT License
+
+Copyright (c) 2023 Claus Rosenberger <git@rocnet.de>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ main.js
@@ -0,0 +1,290 @@
+'use strict'
+
+/*
+ * Created with @iobroker/create-adapter v2.3.0
+ */
+
+/*
+ TODO
+ ====
+ - more data-types
+*/
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require('@iobroker/adapter-core')
+
+// Load your modules here, e.g.:
+const DomiqClient = require('./lib/domiqClient.js')
+
+let domiqClient
+
+class Domiqbase extends utils.Adapter {
+  /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+  constructor (options) {
+    super({
+      name: 'domiqbase'
+    })
+    this.on('ready', this.onReady.bind(this))
+    this.on('stateChange', this.onStateChange.bind(this))
+    this.on('unload', this.onUnload.bind(this))
+    this.foreignStates = []
+    this.stateMapping = []
+    this.domiqClient = undefined
+    this.me = undefined
+  }
+
+  /*
+     * Is called when databases are connected and adapter received configuration.
+     */
+  async onReady () {
+    // Initialize your adapter here
+    const hostname = this.config.hostname
+    const port = this.config.port
+    const domiqWhitelist = []
+    let initialState
+    const self = this
+    //this.me = 'system.adapter.' + this.name + '.' + this.instance
+    this.me = this.name + '.' + this.instance
+
+    // The adapters config (in the instance object everything under the attribute "native") is accessible via
+    this.log.info('hostname: ' + hostname)
+    this.log.info('port: ' + port)
+    // read domiq regex table
+    if (Array.isArray(this.config.domiqregex) && this.config.domiqregex.length) {
+      this.config.domiqregex.forEach(function (item, index) {
+        self.log.info('config  regex[' + index + ']: ' + JSON.stringify(item))
+        if ('regex' in item && 'read' in item && 'write' in item && 'datatype' in item) {
+          domiqWhitelist.push({
+            regex: new RegExp(item.regex),
+            read: item.read,
+            write: item.write,
+            datatype: item.datatype
+          }
+          )
+        }
+      })
+    };
+
+    // read foreign states table
+    if (Array.isArray(this.config.regexentries) && this.config.regexentries.length) {
+      this.config.regexentries.forEach(function (item, index) {
+        if ('targetvariable' in item && 'regularexpression' in item && 'replacestring' in item) {
+          self.log.info('config  foreign state[' + index + ']: ' + JSON.stringify(item))
+          self.foreignStates.push({
+            search: RegExp(item.regularexpression),
+            replace: item.targetvariable + '.' + item.replacestring
+          }
+          )
+        }
+      })
+    };
+
+    // subscribe states
+    this.subscribeStates(this.name + '.' + this.instance + '.*')
+
+    // connect to domiq base
+    initialState = true
+    this.domiqClient = new DomiqClient({ host: hostname, port })
+    this.domiqClient.connect()
+
+    this.domiqClient.on('connect', function () {
+      self.log.info('Domiq connected')
+      initialState = false
+    })
+
+    this.domiqClient.on('close', function () {
+      self.log.info('Connection closed')
+    })
+
+    this.domiqClient.on('reconnectFailed', function () {
+      self.log.warn('Domiq automatically reconnect failed, restarting ...')
+      domiqClient.connect()
+    })
+
+    this.domiqClient.on('error', function (e) {
+      self.log.error('error: ' + e)
+    })
+
+    this.domiqClient.on('event', async (address, value) => {
+      if (!initialState) {
+        initialState = true
+        this.domiqClient.writeRaw('?');
+      }
+
+      // create objects for elements on the whitelist
+      domiqWhitelist.forEach(async function (item, _) {
+        let newValue;
+        const result = item.regex.exec(address)
+        if (Array.isArray(result) && result.length) {
+          await self.setObjectNotExistsAsync(address, {
+            type: 'state',
+            common: {
+              name: address,
+              type: item.datatype,
+              role: 'value',
+              read: item.read,
+              write: item.write
+            },
+            native: {}
+          })
+
+          if (item.datatype === 'number') {
+            newValue = parseFloat(Number(value).toFixed(11))
+          } else if (item.datatype === 'boolean') {
+            newValue = { 0: false, 1: true }[value]
+          } else  {  // string
+            newValue = value 
+          }                              
+          await self.setStateAsync(address, { val: newValue, ack: true })
+          self.subscribeStates(address)
+          
+        }
+      })
+
+      // write state changes from Base to the origin
+      const result = this.stateMapping.find(e => e.target === address)
+      if (result) {
+        this.getForeignObject(result.origin, (errobj, obj) => {
+          if (errobj) {
+            this.log.error('error getForeignObject: ' + JSON.stringify(errobj))
+          } else if (obj) {
+            this.getForeignState(result.origin, (errstate, state) => {
+              let newValue
+              if (errstate) {
+                this.log.error('error getforeignState: ' + JSON.stringify(errstate))
+              } else if (state) {
+                if (obj.common.write) {
+                  if (obj.common.type === 'number') {
+                    if (Number(state.val).toFixed(11) !== Number(value).toFixed(11)) {
+                      newValue = { val: parseFloat(Number(value).toFixed(11)) }
+                    }
+                  } else if (obj.common.type === 'string') {
+                    if (state.val !== value) {
+                      newValue = { val: value }
+                    }
+                  } else if (obj.common.type === 'boolean') {
+                    newValue = { val: { 0: false, 1: true }[value] }
+                  }
+                  if (newValue) {
+                    this.setForeignStateAsync(result.origin, { val: newValue.val, ack: false })
+                  }
+                } else {
+                  this.log.info('value ' + value + ' cannot be written because ' + result.origin + ' is read-only')
+                }
+              }
+            })
+          }
+        })
+      }
+    })
+
+    // read foreign objects table and subscribe foreign states
+    if (Array.isArray(this.config.foreignobjects) && this.config.foreignobjects.length) {
+      this.config.foreignobjects.forEach((item, index) => {
+        if ('searchstring' in item) {
+          // TODO - check if origin does exist or check it in the admin panel, or both
+          this.log.info('config  foreign object[' + index + ']: ' + item.searchstring)
+          this.subscribeForeignStates(item.searchstring)
+          // initial populate Domiq Base
+          this.getForeignStates(item.searchstring, (err, states) => {
+            if (err) {
+              this.log.error('error getForeignStates: ' + JSON.stringify(err))
+            } else {
+              Object.keys(states).forEach((item, _) => {
+                this.onStateChange(item, states[item])
+              })
+            }
+          })
+        }
+      })
+    };
+  }
+
+  /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+  onUnload (callback) {
+    try {
+      // Here you must clear all timeouts or intervals that may still be active
+      // clearTimeout(timeout2);
+      // ...
+      // clearInterval(interval1);
+
+      callback()
+    } catch (e) {
+      callback()
+    }
+  }
+
+  /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+
+  onStateChange (id, state) {
+    if (state) {
+      // The state was changed*/
+      let newValue
+      let idLocation = id.split('.').filter((el, idx) => idx < 2).join('.')
+
+      if (idLocation !== this.me) {
+        this.getForeignObject(id, (err, obj) => {
+          if (err) {
+            this.log.error('error getForeignObject: ' + JSON.stringify(err))
+          } else if (obj) {
+            // send state changes back to the Base (if the source of the state is a foreign adapter)
+            let result = this.stateMapping.find(e => e.origin === id)
+            if (!result) {
+              this.foreignStates.forEach((item, _) => {
+                if (id.match(item.search)) {
+                  result = { origin: id, target: id.replace(item.search, item.replace) }
+                  this.stateMapping.push(result)
+                  this.log.info('created new state mapping: ' + result.origin + ' > ' + result.target)
+                }
+              })
+            }
+            // var result = this.foreignStates.find(e => e.origin === id);
+            if (result) {
+              if (obj.common.type === 'boolean') {
+                newValue = { false: '0', true: '1' }[state.val]
+              } else {
+                newValue = state.val
+              }
+              // this.log.info(result.origin + ' > ' + result.target + ' = ' + newValue );
+              this.domiqClient.write(result.target, newValue)
+            }
+          }
+        })
+      } else {
+        // handle state updates sent from ioBroker to the  Base
+        if (state.ack === false) {
+          if (obj.common.type == 'boolean') {
+            newValue = { false: '0', true: '1' }[state.val]
+          } else {
+            newValue = state.val
+          }
+          this.domiqClient.write(id.split('.').filter((el, idx) => idx > 1).join('.'), newValue)
+        }
+      }
+    } else {
+      // The state was deleted
+      this.log.info(`state ${id} deleted`)
+    }
+  }
+}
+
+if (require.main !== module) {
+  // Export the constructor in compact mode
+  /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+  module.exports = (options) => new Domiqbase(options)
+} else {
+  // otherwise start the instance directly
+  new Domiqbase()
+}
